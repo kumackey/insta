284f4f0c4e58a8dc2d75b57d13e36df94b6b304d
kumackey <kumak1t09e0@gmail.com>
2019-12-23 21:22:48 +0900

    slimの導入とg controller初期設定

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,20 @@ module Insta
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.generators do |g|
+      g.template_engine = :slim
+      g.assets false
+      g.skip_routes true
+      g.helper false
+      # g.test_framework :rspec,   # テストフレームワークはrspecを使用の場合
+      #                  view_specs: false,
+      #                  helper_specs: false,
+      #                  routing_specs: false,
+      #                  controller_specs: true,
+      #                  request_specs: false,
+      #                  fixtures: true
+      g.fixture_replacement :factory_bot, dir: 'spec/factories' # fixtureはfactory_botでディレクトリを変更
+    end
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
