251819430296d07ab5e6e9c6c745fb467f49a0c7
kumackey <kumak1t09e0@gmail.com>
2019-12-26 21:09:08 +0900

    sessions controllerの導入
    rootをsessions#newへ変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  root 'top#index'
+  root 'sessions#new'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   resources :users
 end
--- test/controllers/top_controller_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class TopControllerTest < ActionDispatch::IntegrationTest
-end
