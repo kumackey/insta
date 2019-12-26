90c9b5f62dfad177dc24869d5654487f1a58552b
kumackey <kumak1t09e0@gmail.com>
2019-12-26 17:28:45 +0900

    rubocop -a

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -19,10 +19,10 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   def setup
-    @user = User.new(name: "Example User", email: "user@example.com")
+    @user = User.new(name: 'Example User', email: 'user@example.com')
   end
 
-  test "should be valid" do
+  test 'should be valid' do
     assert @user.valid?
   end
 end
