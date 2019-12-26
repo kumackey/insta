6f8a5cbb38cb73c7ae933d9aabd3641ca2a7928e
kumackey <kumak1t09e0@gmail.com>
2019-12-26 17:18:37 +0900

    nameカラムの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_23_123004) do
+ActiveRecord::Schema.define(version: 2019_12_26_080615) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", null: false
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2019_12_23_123004) do
     t.string "salt"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
