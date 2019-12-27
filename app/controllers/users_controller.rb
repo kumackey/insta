600a6c93e537e87d7c1dc0ce59ae55542fbb2656
kumackey <kumak1t09e0@gmail.com>
2019-12-27 19:09:12 +0900

    createの挙動を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,19 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  def create; end
+  def create
+    @user = User.new(params[user_params])
+    if @user.save
+      render 'new'
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
+  end
 end
