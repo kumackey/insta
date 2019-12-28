f728af7612c9aae3037197c1a993ed650f3420c0
kumackey <kumak1t09e0@gmail.com>
2019-12-28 13:16:47 +0900

    sorceryによるsession導入

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,22 @@
 class SessionsController < ApplicationController
-  def new; end
+  skip_before_action :require_login, only: [:new, :create]
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = login(params[:email], params[:password])
+    if @user
+      redirect_back_or_to root_path, success: 'ログインしました'
+    else
+      flash.now[:danger] = 'ログインに失敗しました'
+      render action: 'new'
+    end
+  end
+
+  def destroy
+    logout
+    redirect_to root_path, success: 'ログアウトしました'
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -19,6 +19,7 @@ class User < ApplicationRecord
   authenticates_with_sorcery!
   validates :name, presence: true
   validates :email, presence: true, uniqueness: { case_sensitive: false }
-  validates :password, confirmation: true
-  validates :password_confirmation, presence: true
+  validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
+  validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
+  validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
 end
