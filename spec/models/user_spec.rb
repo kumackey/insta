7b60c1f12e80c1a585a73eae44a6c08da3b18fff
kumackey <kumak1t09e0@gmail.com>
2020-01-06 07:04:24 +0900

    letの使用

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  let(:user) { FactoryBot.create(:user) }
   it "有効なファクトリを持つこと" do
-    expect(FactoryBot.build(:user)).to be_valid
+    expect(user).to be_valid
   end
 end
