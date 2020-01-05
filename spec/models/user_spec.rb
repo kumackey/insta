508946a2c585e063eb0044cf02f941c6fb20a6b4
kumackey <kumak1t09e0@gmail.com>
2020-01-06 06:55:47 +0900

    ファクトリの作成と、それが機能することを確認

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "有効なファクトリを持つこと" do
+    expect(FactoryBot.build(:user)).to be_valid
+  end
 end
