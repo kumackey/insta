a60d6d9b33f0ab3def7bdfd711ed5245c7ba515d
kumackey <kumak1t09e0@gmail.com>
2020-01-09 18:20:35 +0900

    既存のミニテストを置き換え

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,4 +5,39 @@ RSpec.describe User, type: :model do
   it "有効なファクトリを持つこと" do
     expect(user).to be_valid
   end
+  
+  it "名前が空白であるときに無効なこと" do
+    user.name = '     '
+    user.valid?
+    expect(user.errors.messages[:name]).to include("を入力してください")
+  end
+
+  describe "名前の最大文字数が100文字" do
+    context "名前が100文字" do
+      let(:user) { FactoryBot.build(:user, name:'a' * 100) }
+      it "有効であること" do
+        expect(user).to be_valid
+      end
+    end
+    context "名前が101文字" do
+      let(:user) { FactoryBot.build(:user, name:'a' * 101) }
+      it "無効であること" do
+        user.valid?
+        expect(user.errors[:name]).to include("は100文字以内で入力してください")
+      end
+    end
+  end
+
+  it "Eメールが空白であるときに無効なこと" do
+    user.email = '     '
+    user.valid?
+    expect(user.errors[:email]).to include("を入力してください")
+  end
+  
+  it "重複したメールアドレスなら無効な状態であること" do
+    FactoryBot.create(:user, email:"dup@example.com")
+    user = FactoryBot.build(:user, email:"dup@example.com")
+    user.valid?
+    expect(user.errors[:email]).to include("はすでに存在します")
+  end
 end
