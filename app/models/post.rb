197b27e5a5e4e01dcf6bd28a9177fad7e2599ea5
kumackey <kumak1t09e0@gmail.com>
2020-01-12 07:15:18 +0900

    投稿テキストのヴァリデーション

--- app/models/post.rb
+++ app/models/post.rb
@@ -22,4 +22,5 @@
 class Post < ApplicationRecord
   belongs_to :user, foreign_key: :user_id
   validates :user_id, presence: true
+  validates :content, presence: true, length: { maximum: 140 }
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -10,4 +10,23 @@ RSpec.describe Post, type: :model do
     post = FactoryBot.build(:post, user_id: nil)
     expect(post).not_to be_valid
   end
+
+  it "テキストの無い投稿は無効なこと" do
+    post = FactoryBot.build(:post, content: nil)
+    expect(post).not_to be_valid
+  end
+  describe "投稿のテキストの文字数が最大140文字" do
+    context "投稿のテキストが140文字のときに" do
+      let(:post) { FactoryBot.create(:post, content: 'a'*140) }
+      it "有効なこと" do
+        expect(post).to be_valid
+      end
+    end
+    context "投稿のテキストが141文字のときに" do
+      it "無効なこと" do
+        post = FactoryBot.build(:post, content: 'a'*141)
+        expect(post).not_to be_valid
+      end
+    end
+  end
 end
