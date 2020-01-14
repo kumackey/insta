086ccb584ffde55011f65a384f56c371473b117d
kumackey <kumak1t09e0@gmail.com>
2020-01-14 21:07:51 +0900

    postsのimagesカラムをjsonに変更(前回失敗してた)

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_13_020056) do
+ActiveRecord::Schema.define(version: 2020_01_14_114548) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "content", null: false
     t.bigint "user_id"
-    t.string "images", null: false
+    t.json "images", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id", "created_at"], name: "index_posts_on_user_id_and_created_at"
