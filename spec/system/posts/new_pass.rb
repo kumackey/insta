46c54bd2a4a6ceb4d78ffee2d83d9758ff633bf1
kumackey <kumak1t09e0@gmail.com>
2020-01-18 13:40:29 +0900

    システムスペックをディレクトリで分ける
    全部テストされるのが時間かかるため

---
+++ spec/system/posts/new_pass.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe 'Posts', type: :system do
+  it '投稿できること' do
+    login_user
+    visit new_post_path
+    attach_file '画像', Rails.root.join('spec', 'factories', 'test1.png')
+    fill_in '本文', with: 'This is an example post'
+    click_button '登録する'
+    expect(page).to have_content '投稿しました'
+    expect(page).to have_content 'This is an example post'
+  end
+end
