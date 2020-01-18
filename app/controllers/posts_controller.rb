e1d61addd03fd4f0ac8ac686be87c7603e2100fa
kumackey <kumak1t09e0@gmail.com>
2020-01-18 16:21:05 +0900

    postsのdestroy導入

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :require_login, only: %i[new create edit update]
+  before_action :require_login, only: %i[new create edit update destroy]
 
   def index
     @posts = Post.all.includes(:user)
@@ -35,6 +35,12 @@ class PostsController < ApplicationController
       flash.now[:danger] = '更新に失敗しました'
       render :edit
     end
+  end
+
+  def destroy
+    @post = current_user.posts.find(params[:id])
+    @post.destroy!
+    redirect_to posts_path, success: '投稿を削除しました'
   end
 
   private
