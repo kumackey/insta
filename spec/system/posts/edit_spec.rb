fdd5e7e4204ad2217f1dc91a8b82b5f7a6df8abf
kumackey <kumak1t09e0@gmail.com>
2020-01-18 16:09:13 +0900

    system specのリファクタリング

--- spec/system/posts/edit_spec.rb
+++ spec/system/posts/edit_spec.rb
@@ -1,9 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :system do
-  it '投稿できること' do
+  it '投稿を編集できること' do
     user = login_user_as(user)
     post = FactoryBot.create(:post, user: user, content: 'This is first post')
+
+    #編集に失敗
+    visit edit_post_path post
+    attach_file '画像', Rails.root.join('spec', 'factories', 'test2.png')
+    fill_in '本文', with: ''
+    click_button '更新する'
+    expect(page).to have_content '更新に失敗しました'
+    visit posts_path
+    expect(page).to have_content 'This is first post'
+
+    #編集に成功
     visit edit_post_path post
     attach_file '画像', Rails.root.join('spec', 'factories', 'test2.png')
     fill_in '本文', with: 'This is second post'
