77bb683c33730ca937210e2216242f130074b102
kumackey <kumak1t09e0@gmail.com>
2020-01-26 10:16:01 +0900

    コメントのテキストのvalidation

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -23,4 +23,5 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
+  validates :content, presence: true, length: { maximum: 140 }
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -37,4 +37,24 @@ RSpec.describe Comment, type: :model do
     comment = FactoryBot.build(:comment, post_id: nil)
     expect(comment).not_to be_valid
   end
+
+  it "テキストの無いコメントは無効なこと" do
+    comment = FactoryBot.build(:comment, content: nil)
+    expect(comment).not_to be_valid
+  end
+
+  describe "コメントのテキストの文字数が最大140文字" do
+    context "コメントのテキストが140文字のときに" do
+      let(:comment) { FactoryBot.create(:comment, content: 'a'*140) }
+      it "有効なこと" do
+        expect(comment).to be_valid
+      end
+    end
+    context "コメントのテキストが141文字のときに" do
+      it "無効なこと" do
+        comment = FactoryBot.build(:comment, content: 'a'*141)
+        expect(comment).not_to be_valid
+      end
+    end
+  end
 end
