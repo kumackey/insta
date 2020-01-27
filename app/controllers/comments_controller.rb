7ff83ee492133b58744403564a7483f223ccc3df
kumackey <kumak1t09e0@gmail.com>
2020-01-27 19:25:57 +0900

    comment#editアクションの実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,13 @@
 class CommentsController < ApplicationController
-  before_action :require_login, only: %i[create destroy]
+  before_action :require_login, only: %i[create edit update destroy]
 
   def create
     @comment = current_user.comments.build(comment_create_params)
     @comment.save
+  end
+
+  def edit
+    @comment = current_user.comments.find(params[:id])
   end
 
   def destroy
