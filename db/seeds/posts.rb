a8b950d00b5beab5b8e1f0298cbb1b7e394e9274
kumackey <kumak1t09e0@gmail.com>
2020-02-01 10:33:45 +0900

    seedsの不具合を修正
    sample0.pngは無いため

--- db/seeds/posts.rb
+++ db/seeds/posts.rb
@@ -1,5 +1,5 @@
 puts 'Start inserting seed "posts" ...'
 User.limit(10).each.with_index(1) do |user, index|
-  post = user.posts.create(content: Faker::Hacker.say_something_smart, images: [open("#{Rails.root}/db/seeds/images/sample#{index}.png"), open("#{Rails.root}/db/seeds/images/sample#{10-index}.png")])
+  post = user.posts.create(content: Faker::Hacker.say_something_smart, images: [open("#{Rails.root}/db/seeds/images/sample#{index}.png"), open("#{Rails.root}/db/seeds/images/sample#{11-index}.png")])
   puts "post#{post.id} has created!"
 end
