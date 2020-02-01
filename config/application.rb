bce5b568cf418b6a1800d54b3d2005158e32a13c
kumackey <kumak1t09e0@gmail.com>
2020-02-01 10:59:37 +0900

    コントローラ生成時にコントローラテストを作らない
    不要なため

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,8 @@ module Insta
       g.test_framework :rspec,
                        view_specs: false,
                        helper_specs: false,
-                       routing_specs: false
+                       routing_specs: false,
+                       controller_specs: false
       g.fixture_replacement :factory_bot, dir: 'spec/factories' # fixtureはfactory_botでディレクトリを変更
     end
 
