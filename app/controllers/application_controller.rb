3200276124bc30a3dff3cb138176f5f3394536d3
kumackey <kumak1t09e0@gmail.com>
2020-02-25 11:45:10 +0900

    q: {content: value}のストロングパラメータ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,18 @@ class ApplicationController < ActionController::Base
   add_flash_types :success, :info, :warning, :danger
 
   def set_search_posts_form
-    @search_form = SearchPostsForm.new
+    @search_form = SearchPostsForm.new(search_post_params)
   end
 
   protected
 
   def not_authenticated
     redirect_to login_path, success: 'ログインしてください'
+  end
+
+  private
+
+  def search_post_params
+    params.fetch(:q, {}).permit(:content)
   end
 end
