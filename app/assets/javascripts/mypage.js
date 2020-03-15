e7a84f9dd72d27ba32c763fbde496fcad66e137d
kumackey <kumak1t09e0@gmail.com>
2020-03-15 13:57:09 +0900

    プレビュー表示

--- app/assets/javascripts/mypage.js
+++ app/assets/javascripts/mypage.js
@@ -10,4 +10,25 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery3
+//= require popper
+//= require rails-ujs
+//= require activestorage
+//= require_tree
+//= require bootstrap-material-design/dist/js/bootstrap-material-design.js
+//= require swiper/js/swiper.js
+//= require swiper.js
 
+function previewFileWithId(selector) {
+  const target = this.event.target;
+  const file = target.files[0];
+  const reader  = new FileReader();
+  reader.onloadend = function () {
+      selector.src = reader.result;
+  }
+  if (file) {
+      reader.readAsDataURL(file);
+  } else {
+      selector.src = "";
+  }
+}
